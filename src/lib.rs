78f82dbdba83d5caafe71bf6becead993e769e01
safranklin <sfranklin@vt.edu>
2022-02-19 14:29:12 -0500

    feat: Add lib tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use std::error::Error;
 use std::fs;
 
+#[derive(Debug, Clone)]
 pub struct Args {
     pub query: String,
     pub file_path: String,
@@ -20,7 +21,9 @@ impl Args {
         let file_path = args[2].clone();
         Ok(Args { query, file_path })
     }
+    
 }
+
 
 pub fn run(args: Args) -> Result<(), Box<dyn Error>> {
     // Read the string, if an error occurs return it to the caller for handling using the '?' operator.
@@ -30,4 +33,62 @@ pub fn run(args: Args) -> Result<(), Box<dyn Error>> {
     
     // Upon success return the unit type '()'
     Ok(())
+}
+
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    #[should_panic]
+    fn args_new_too_few_args() {
+        match Args::new(
+            &vec!(
+                String::from("./sgrep"),
+                String::from("<query>")
+            )
+        ) {
+            Ok(val) => println!("Successfully initialized! {} {}", val.query, val.file_path),
+            Err(e) => panic!("{}", e)
+        }
+    }
+
+    #[test]
+    #[should_panic]
+    fn args_new_too_many_args() {
+        match Args::new(
+            &vec!(
+                String::from("./sgrep"),
+                String::from("<query>"),
+                String::from("<file_path>"),
+                String::from("<extra_arg>")
+            )
+        ) {
+            Ok(val) => println!("Successfully initialized! {} {}", val.query, val.file_path),
+            Err(e) => panic!("{}", e)
+        }
+    }
+
+    #[test]
+    fn invalid_file() {
+        if let Ok(args) = Args::new(
+            &vec!(
+                String::from("./sgrep"),
+                String::from("<query>"),
+                String::from("./some_path_that_does_not_exist")
+            )
+        ) {
+            if let Err(e) = run(args) {
+                assert!(e.is::<std::io::Error>());
+            } else {
+                panic!("File path should not be valid.");
+            }
+        }
+        else {
+            panic!("Couldn't instantiate valid constructor.");
+        }
+    }
+
 }
